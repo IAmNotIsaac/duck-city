3f65585bf68d6a5e6fb85bad04ae353e8fd64344
IAmNotIsaac <isaacjulianastell@gmail.com>
2023-02-11 22:08:32 -0800

    made avian mech chase players

--- scenes/avian.gd
+++ scenes/avian.gd
@@ -4,13 +4,16 @@ extends CharacterBody3D
 
 enum MoveStates {
 	GROUND,
-	AIR
+	AIR,
+	MOVE_STATES_MAX
 }
 
 enum AIStates {
 	IDLE,
 	GENERIC_OBJECTIVE,
-	URGENT_OBJECTIVE
+	URGENT_OBJECTIVE,
+	CHASE_TARGET,
+	AI_STATES_MAX
 }
 
 
@@ -18,17 +21,22 @@ const _GRAVITY := 20.0
 const _ACCELERATION := 0.5
 const _DEACCELERATION := 0.25
 
+@export var _target_type := Constants.AvianType.Avian
 @export var _generic_move_speed := 0.0
 @export var _urgent_move_speed := 0.0
+@export var _chase_move_speed := 0.0
 
 var _mstate := StateMachine.new(self, MoveStates, MoveStates.GROUND, "M")
 var _aistate := StateMachine.new(self, AIStates, AIStates.IDLE, "AI")
 
 var _acceleration := Vector3.ZERO
 var _move_speed := 0.0
+var _target : AvianTarget
 
 @onready var _n_agent := $NavigationAgent3D
 @onready var _n_gimbal := $Gimbal
+@onready var _n_eyes := $Gimbal/Eyes
+@onready var _n_eyes_check := $EyesCheck
 
 
 ## Private methods ##
@@ -39,6 +47,7 @@ func _ready() -> void:
 
 
 func _process(delta : float) -> void:
+#	$DebugStateLabel.text = AIStates.keys()[_aistate.get_state()] + "\n" + MoveStates.keys()[_mstate.get_state()]
 	_aistate.process(delta)
 	
 	if Input.is_action_pressed("interact"):
@@ -47,6 +56,29 @@ func _process(delta : float) -> void:
 
 func _physics_process(delta : float) -> void:
 	_mstate.process(delta)
+
+
+func _accelerate_on_path() -> void:
+	var tpos = _n_agent.get_next_path_position()
+	var direction := global_position.direction_to(tpos)
+	_acceleration = direction * _ACCELERATION
+
+
+func _rotate_on_path() -> void:
+	var tpos = _n_agent.get_next_path_position()
+	var direction := global_position.direction_to(tpos)
+	var dir := Vector2(direction.x, direction.z).normalized()
+	_n_gimbal.rotation.y = atan2(-dir.y, dir.x)
+
+
+func _is_target_visible(target : AvianTarget) -> bool:
+	if target.is_enabled() and _n_eyes.is_position_in_frustum(target.get_global_position()):
+		_n_eyes_check.set_target_position(target.get_global_position() - _n_eyes_check.get_global_position())
+		await get_tree().process_frame
+		
+		return _n_eyes_check.get_collider() == target.get_collider()
+	
+	return false
 
 
 ## Public methods ##
@@ -58,6 +90,11 @@ func goto_objective(pos : Vector3, urgent := false) -> void:
 		_aistate.switch(AIStates.GENERIC_OBJECTIVE)
 	else:
 		_aistate.switch(AIStates.URGENT_OBJECTIVE)
+
+
+func chase_target(target : AvianTarget) -> void:
+	_target = target
+	_aistate.switch(AIStates.CHASE_TARGET)
 
 
 ## State processes (MoveStates) ##
@@ -95,22 +132,50 @@ func _sl_M_GROUND() -> void:
 
 func _sp_AI_IDLE(_delta : float) -> void:
 	_acceleration = -velocity * _DEACCELERATION
+	
+	for t in get_tree().get_nodes_in_group(Constants.AvianType.keys()[_target_type] + "_target"):
+		if await _is_target_visible(t):
+			chase_target(t)
+			return
 
 
 func _sp_AI_GENERIC_OBJECTIVE(_delta : float) -> void:
-	var tpos = _n_agent.get_next_path_position()
-	var direction := global_position.direction_to(tpos)
-	_acceleration = direction * _ACCELERATION
-	
-	var dir := Vector2(direction.x, direction.z).normalized()
-	_n_gimbal.rotation.y = atan2(-dir.y, dir.x)
+	_accelerate_on_path()
+	_rotate_on_path()
 	
 	if _n_agent.is_navigation_finished():
 		_aistate.switch(AIStates.IDLE)
+		return
+	
+	for t in get_tree().get_nodes_in_group(Constants.AvianType.keys()[_target_type] + "_target"):
+		if await _is_target_visible(t):
+			chase_target(t)
+			return
 
 
-func _sp_AI_URGENT_OBJECTIVE(delta : float) -> void:
-	_sp_AI_GENERIC_OBJECTIVE(delta)
+func _sp_AI_URGENT_OBJECTIVE(_delta : float) -> void:
+	_accelerate_on_path()
+	_rotate_on_path()
+	
+	if _n_agent.is_navigation_finished():
+		_aistate.switch(AIStates.IDLE)
+		return
+
+
+func _sp_AI_CHASE_TARGET(_delta : float) -> void:
+	if _target == null:
+		_aistate.switch(AIStates.IDLE)
+		return
+	
+	if await _is_target_visible(_target):
+		_n_agent.set_target_position(_target.get_global_position())
+	
+	elif _n_agent.is_navigation_finished():
+		_aistate.switch(AIStates.IDLE)
+		return
+	
+	_accelerate_on_path()
+	_rotate_on_path()
 
 
 ## State [un]loading (AIStates) ##
@@ -122,3 +187,7 @@ func _sl_AI_GENERIC_OBJECTIVE() -> void:
 
 func _sl_AI_URGENT_OBJECTIVE() -> void:
 	_move_speed = _urgent_move_speed
+
+
+func _sl_AI_CHASE_TARGET() -> void:
+	_move_speed = _chase_move_speed
---
+++ scenes/avian_target.gd
@@ -0,0 +1,27 @@
+class_name AvianTarget
+extends Node3D
+
+
+@export var _enabled := true
+@export var _avian_type := Constants.AvianType.Avian
+@export_node_path("PhysicsBody3D") var collider_path = ^".."
+
+
+func _init() -> void:
+	add_to_group(Constants.AvianType.keys()[_avian_type] + "_target")
+
+
+func enable() -> void:
+	_enabled = true
+
+
+func disable() -> void:
+	_enabled = false
+
+
+func is_enabled() -> bool:
+	return _enabled
+
+
+func get_collider() -> PhysicsBody3D:
+	return get_node(collider_path)
